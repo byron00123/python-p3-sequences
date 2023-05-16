b83f531af728cad8676b9fec5b30089b60abad2f
Byron Omondi <omondibyron25@gmail.com>
2023-05-16 12:05:08 +0300

    complete assingment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = []
+
+    if length == 0:
+        print(sequence)
+        return
+
+    # Generate the Fibonacci sequence
+    a, b = 0, 1
+    sequence.append(a)
+
+    while len(sequence) < length:
+        sequence.append(b)
+        a, b = b, a + b
+
+    # Print the Fibonacci sequence
+    print(sequence)
     pass
